0d6b18f533eb369d7044d77e37f64bf0f77b0205
misantron <misantron@gmail.com>
2015-06-11 18:01:59 +0300

    Remove api documentation

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Utility\\Exception\\ExtensionNotLoadedException"],["c","Utility\\Exception\\InvalidArgumentException"],["c","Utility\\Exception\\NonStaticCallException"],["c","Utility\\Exception\\OutOfRangeException"],["c","Utility\\Exception\\RuntimeException"],["c","Utility\\Translation\\UAbstractTranslation"],["c","Utility\\Translation\\UArrayTranslation"],["c","Utility\\Translation\\UStringTranslation"],["c","Utility\\Translation\\UTimeTranslation"],["c","Utility\\UAbstract"],["c","Utility\\UArray"],["c","Utility\\UString"],["c","Utility\\UTime"]];
